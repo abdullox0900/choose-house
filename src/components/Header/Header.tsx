fa06041c0a2dafffef840aed907c2b3763ff20ef
abdullox0900 <abdullox0900@gmail.com>
2024-08-07 22:49:48 +0500

    feat: header responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,60 +1,78 @@
 'use client'
 
-import React, { useState } from 'react';
-import Link from 'next/link';
-import LocalSwitcher from './LocalSwitcher';
-import Image from 'next/image';
-
 import SiteLogo from '@/assets/svg/site-logo.svg'
-import { useTranslations } from 'next-intl';
-import HamburgerMenu from './HamburgerMenu';
+import { useTranslations } from 'next-intl'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useEffect, useRef, useState } from 'react'
+import { animated, useSpring } from 'react-spring'
+import HamburgerMenu from './common/HamburgerMenu'
+import LocalSwitcher from './common/LocalSwitcher'
 
 const Header: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const menuRef = useRef<HTMLDivElement>(null)
+  const headerRef = useRef<HTMLDivElement>(null)
+  const t = useTranslations('header')
 
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const t = useTranslations('header');
+  const menuAnimation = useSpring({
+    height: isMenuOpen ? (menuRef.current?.scrollHeight || 0) : 0,
+    marginTop: isMenuOpen ? 20 : 0,
+    opacity: isMenuOpen ? 1 : 0,
+    config: {
+      tension: 400,
+      friction: 26,
+      clamp: true  // Bu animatsiyani to'xtatadi agar maqsadga yetsa
+    }
+  })
+
+  useEffect(() => {
+    if (headerRef.current) {
+      headerRef.current.style.height = isMenuOpen
+        ? `${headerRef.current.offsetHeight + (menuRef.current?.scrollHeight || 0)}px`
+        : 'auto'
+    }
+  }, [isMenuOpen])
 
   return (
-    <header className="fixed left-1/2 -translate-x-1/2 top-5 w-[95%] rounded-[10px] bg-blue-500 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-white text-2xl font-bold">
-          <Image src={SiteLogo} alt='site-logo' />
-        </Link>
-        <nav className='flex items-center gap-[15px]'>
-          <ul className={`${isMenuOpen ? '' : ''} flex space-x-4 max-tablet:hidden`}>
-            <li>
-              <Link href="/kategoriyalar" className="text-white hover:text-blue-200">
-                {t('categories')}
-              </Link>
-            </li>
-            <li>
-              <Link href="/halol-nasiya" className="text-white hover:text-blue-200">
-                {t('halal_installment')}
-              </Link>
-            </li>
-            <li>
-              <Link href="/chegirma" className="text-white hover:text-blue-200">
-              {t('discount')}
-              </Link>
-            </li>
-            <li>
-              <Link href="/premium-house" className="text-white hover:text-blue-200">
-                {t('premium_house')}
-              </Link>
-            </li>
-            <li>
-              <Link href="/shaxardan-tashqari" className="text-white hover:text-blue-200">
-                {t('outside_city')}
-              </Link>
-            </li>
-          </ul>
-        <LocalSwitcher />
-        <HamburgerMenu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
-
-        </nav>
+    <header ref={headerRef} className="fixed left-1/2 -translate-x-1/2 top-5 w-[95%] rounded-[10px] bg-blue-500 p-4 overflow-hidden transition-all duration-300 ease-in-out">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <Link href="/" className="text-white text-2xl font-bold">
+            <Image className='max-mobile-m:w-[110px]' src={SiteLogo} alt='site-logo' />
+          </Link>
+          <div className='flex items-center gap-[15px] max-mobile-m:gap-[8px]'>
+            <nav className='max-tablet:hidden'>
+              <ul className="flex items-center gap-[30px]">
+                {['categories', 'halal_installment', 'discount', 'premium_house', 'outside_city'].map((item) => (
+                  <li key={item}>
+                    <Link href={`/${item}`} className="text-white hover:text-blue-200">
+                      {t(item)}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+            <LocalSwitcher />
+            <HamburgerMenu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
+          </div>
+        </div>
+        <animated.div style={menuAnimation} className="tablet:hidden">
+          <nav ref={menuRef}>
+            <ul className="flex flex-col space-y-2 h-[180px]">
+              {['categories', 'halal_installment', 'discount', 'premium_house', 'outside_city'].map((item) => (
+                <li key={item}>
+                  <Link href={`/${item}`} className="text-white hover:text-blue-200">
+                    {t(item)}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </animated.div>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        'primary': '#FF0000',
-        'secondary': '#00FF00',
-        'accent': '#0000FF',
-      },
-      screens: {
-        'max-tablet': { 'raw': '(max-width: 1024px)' },
-      }
-    },
-  },
-  plugins: [],
-};
-export default config;
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			colors: {
+				primary: '#FF0000',
+				secondary: '#00FF00',
+				accent: '#0000FF',
+			},
+			screens: {
+				'max-tablet': { raw: '(max-width: 1024px)' },
+				'max-tablet-m': { raw: '(max-width: 765px)' },
+				'max-mobile-l': { raw: '(max-width: 425px)' },
+				'max-mobile-m': { raw: '(max-width: 375px)' },
+			},
+		},
+	},
+	plugins: [],
+}
+export default config
